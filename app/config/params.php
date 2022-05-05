f586e460dd162ab97f84e4c8bab8bf80fb2c2122
Dde <davidedetiege.webdev@gmail.com>
2022-05-05 15:17:34 +0200

    Structure MVC de base

---
+++ app/config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+// PARAMETRES DU TEMPLATE
+$content = ' ';
+
+// PARAMETRES DE CONNEXION
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'readit_2020');
+define('DB_USER', 'root');
+define('DB_PWD', 'root');
---
+++ app/routers/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+// ROUTE PAR DEFAUT
+// PATTERN: /?contact
+// CTRL: postControleur
+// ACTION : Index
+
+//if (isset($_GET["postID"])):
+//    include_once '../app/controllers/postsController.php';
+//    showAction($connexion, $_GET['postID']);   
+
+// ROUTE PAR DEFAUT: liste des posts
+// PATTERN: ?comments=add
+// CTRL: postsController
+// ACTION: index
+//else:
+//    include_once '../app/controllers/postsController.php';
+//    indexAction($connexion);
+
+//endif;
